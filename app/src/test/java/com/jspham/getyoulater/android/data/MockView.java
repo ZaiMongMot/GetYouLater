1dccb19cbce54ec68cbd4fbe891e3514236700ea
Pham Van <thang_phamvan@nuance.com>
2018-05-16 11:54:47 +0700

    Add exist files

---
+++ app/src/test/java/com/jspham/getyoulater/android/data/MockView.java
@@ -0,0 +1,11 @@
+package com.jspham.getyoulater.android.data;
+
+import android.content.Context;
+import android.view.View;
+
+public class MockView extends View {
+
+    public MockView(Context context) {
+        super(context);
+    }
+}
